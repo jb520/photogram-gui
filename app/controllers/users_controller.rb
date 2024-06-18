232b40129820858c15dea4919ca228eb0ac6c28c
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-18 16:27:26 +0000

    add formatting to user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    render({ :template => "user_templates/index.html.erb"})
+    render({ :template => "user_templates/index" })
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   #User Pages
   get("/users", { :controller => "users", :action => "index" })
-  get("/users/:path_id", { :controller => "users", :action => "show" })
+  #get("/users/:path_id", { :controller => "users", :action => "show" })
 
   #Photo Pages
-  get("/photos", { :controller => "photos", :action => "index" })
-  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  #get("/photos", { :controller => "photos", :action => "index" })
+  #get("/photos/:path_id", { :controller => "photos", :action => "show" })
 
 end
