6d6e4f6a7eb58979e0124c707ed5a599967500e8
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-18 17:23:08 +0000

    update photoscontroller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,1 @@
+class PhotosController < ApplicationController
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index" })
+
   #User Pages
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
 
   #Photo Pages
-  #get("/photos", { :controller => "photos", :action => "index" })
+  get("/photos", { :controller => "photos", :action => "index" })
   #get("/photos/:path_id", { :controller => "photos", :action => "show" })
 
 end
