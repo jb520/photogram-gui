e60d2287b1f3675b330a35fd0bd221562b1c69aa
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-18 20:50:43 +0000

    add create photo form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,4 +11,27 @@ class PhotosController < ApplicationController
     @the_photo = Photo.where({ :id => url_id }).at(0)
     render({ :template => "photo_templates/show"})
   end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_photo = Photo.where({ :id => the_id }).at(0)
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def create
+    p_image = params.fetch("input_image")
+    p_caption = params.fetch("input_caption")
+    p_owner_id = params.fetch("input_owner_id")
+
+    a_new_photo = Photo.new
+    a_new_photo.image = p_image
+    a_new_photo.caption = p_caption
+    a_new_photo.owner_id = p_owner_id
+
+    a_new_photo.save
+
+    redirect_to("/photos/" + a_new_photo.id.to_s)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   #Photo Pages
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy"})
+  post("/insert_photo_record", { :controller => "photos", :action => "create" })
 
 end
